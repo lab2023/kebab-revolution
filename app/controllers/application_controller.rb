87026f9ac42e56c22c942158da220f91f6d0bfad
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-12 17:06:16 +0200

    Issue #2 Create a response hash for ext js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  @@response = {:success => true}
+
+  protected
+
+  def add_notice type, message
+    notice = [:type => type, :message => message]
+    if @@response.has_key?(:notice)
+      @@response[:notice] += notice
+    else
+      @@response[:notice] = notice
+    end
+  end
+
+  def add_error id, message
+    @@response[:success] = false
+    error = [:id => id, :message => message]
+    if @@response.has_key?(:error)
+      @@response[:error] += error
+    else
+      @@response[:error] = error
+    end
+  end
 end
