76291843614df97431f8a1b1ef4efad534880873
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-12 17:09:49 +0200

    Issue #2 typo fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,22 +5,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def add_notice type, message
-    notice = [:type => type, :message => message]
-    if @@response.has_key?(:notice)
-      @@response[:notice] += notice
-    else
-      @@response[:notice] = notice
+    def add_notice type, message
+      notice = [:type => type, :message => message]
+      if @@response.has_key?(:notice)
+        @@response[:notice] += notice
+      else
+        @@response[:notice] = notice
+      end
     end
-  end
 
-  def add_error id, message
-    @@response[:success] = false
-    error = [:id => id, :message => message]
-    if @@response.has_key?(:error)
-      @@response[:error] += error
-    else
-      @@response[:error] = error
+    def add_error id, message
+      @@response[:success] = false
+      error = [:id => id, :message => message]
+      if @@response.has_key?(:error)
+        @@response[:error] += error
+      else
+        @@response[:error] = error
+      end
     end
-  end
 end
