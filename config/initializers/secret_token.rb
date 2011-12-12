3ce2d474fb90eb2b7b43b0afa7ffd8300ad6aadc
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-12 16:39:26 +0200

    Issue #1 install rails 3.1.x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KebabServerRor::Application.config.secret_token = 'bd27c0417c3adbfbc44b55855fbc14cecd022813f43d33a089a44fe21f082dcdea6465a66cb098341dbb123fcf229512892699392d3a354761a9fee6ea21a360'
