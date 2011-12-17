a73535dff63af1d65870787e1fa67d02fe8d64fd
ooo <ooo@hermes.(none)>
2011-12-17 18:23:08 +0200

    Issue #17 remove set_i18n_locale_from_accept_language_header from application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,16 +15,16 @@ class ApplicationController < ActionController::Base
       if Tenant.find_by_host(request.host) != nil
         @tenant = Tenant.current = Tenant.find_by_host!(request.host)
       else
-        @@response[:success] = false
+        @@response = {:success => false}
         add_notice 'ERR', I18n.t('notice.invalid_tenant')
-        render json: @@response
+        render json: @@response, status: :not_found
       end
     end
 
     def add_notice type, message
       notice = [:type => type, :message => message]
       if @@response.has_key?(:notice)
-        @@response[:notice] += notice
+        @@response[:notice] += notice unless @@response[:notice].include?(notice) || @@response[:notice] == notice
       else
         @@response[:notice] = notice
       end
@@ -34,14 +34,14 @@ class ApplicationController < ActionController::Base
       @@response[:success] = false
       error = [:id => id, :message => message]
       if @@response.has_key?(:error)
-        @@response[:error] += error
+        @@response[:error] += error unless @@response[:error].include?(error) || @@response[:error] == error
       else
         @@response[:error] = error
       end
     end
 
     def set_i18n_locale
-      I18n.locale = set_i18n_locale_from_param || set_i18n_locale_from_session || set_i18n_locale_from_accept_language_header || I18n.default_locale
+      I18n.locale = set_i18n_locale_from_param || set_i18n_locale_from_session || I18n.default_locale
     end
 
     def set_i18n_locale_from_param
@@ -57,14 +57,6 @@ class ApplicationController < ActionController::Base
         if I18n.available_locales.include?(session[:locale].to_sym)
           session[:locale]
         end
-      end
-    end
-
-    def set_i18n_locale_from_accept_language_header
-      accept_language_header = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-      if I18n.available_locales.include?(accept_language_header.to_sym)
-        accept_language_header = 'en' if accept_language_header == 'en'
-        accept_language_header
       end
     end
 end
