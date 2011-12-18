09fc7c5edd7da322de271e535fc60d4d6a1315cb
ooo <ooo@hermes.(none)>
2011-12-19 00:39:31 +0200

    Issue #10 #11 add service and  app models association

--- app/models/privilege.rb
+++ app/models/privilege.rb
@@ -1,7 +1,10 @@
 class Privilege < ActiveRecord::Base
   has_and_belongs_to_many :roles
-  validates   :sys_name,  :presence => true,
-                          :uniqueness => true
+  has_and_belongs_to_many :apps
+  has_many                :services
+
+  validates   :sys_name,  :presence => true, :uniqueness => true
   validates   :name,      :presence => true
+
   translates  :name, :info, :fallbacks_for_empty_translations => true
 end
