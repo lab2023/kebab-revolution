3f33bee7fdbfa276273992d23660603450b0bf23
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-21 02:25:12 +0200

    Issue #10 improve Apps model

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,6 +1,7 @@
 class App < ActiveRecord::Base
   has_and_belongs_to_many :privileges
 
-  validates :sys_name, :presence => true, :uniqueness => true
-  validates :department,     :presence => true
+  validates  :sys_name,       :presence => true, :uniqueness => true
+  validates  :sys_department, :presence => true
+  translates :name, :fallbacks_for_empty_translations => true
 end
--- db/migrate/20111218194504_create_apps.rb
+++ db/migrate/20111218194504_create_apps.rb
@@ -1,8 +1,35 @@
 class CreateApps < ActiveRecord::Migration
-  def change
+  def up
+
     create_table :apps do |t|
       t.string :sys_name
-      t.string :department
+      t.string :sys_department
     end
+
+    App.create_translation_table! :name => :string
+
+    # add foreign key and make role_id not null
+    execute <<-SQL
+      ALTER TABLE  `app_translations`
+        ADD CONSTRAINT fk_apps_app_translations_id
+        FOREIGN KEY (  `app_id` ) REFERENCES  `apps` ( `id` )
+        ON DELETE RESTRICT ON UPDATE RESTRICT
+    SQL
+
+    # add role_id not null
+    execute <<-SQL
+      ALTER TABLE  `app_translations` CHANGE  `app_id`  `app_id` INT( 11 ) NOT NULL
+    SQL
+
+  end
+
+  def down
+    # drop foreign key role_id
+    execute <<-SQL
+      ALTER TABLE `app_translations` DROP FOREIGN KEY fk_apps_app_translations_id
+    SQL
+
+    App.drop_translation_table!
+    drop_table :apps
   end
 end
