5e11cca9ce379697447ec36158f7e27399a99ce0
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-27 00:29:05 +0200

    Issue #30 add name field to return json

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -2,7 +2,7 @@ class TenantsController < ApplicationController
 
   def register
     @@response[request_forgery_protection_token] = form_authenticity_token
-    @@response['tenant'] = Tenant.select('id, host').find_by_host!(request.host)
+    @@response['tenant'] = Tenant.select('id, host, name').find_by_host!(request.host)
     render json: @@response, callback: params[:callback]
   end
 end
