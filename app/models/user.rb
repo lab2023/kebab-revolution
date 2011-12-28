4b9e4acbc096b587587c5dd5499710a2e6eeda83
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-28 18:57:55 +0200

    Issue #34 remove devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,24 @@
 class User < TenantScopedModel
-  devise :database_authenticatable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
-
+  has_secure_password
   has_and_belongs_to_many :roles
 
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  validates :email,     :presence => {:on => :create},
+                        :uniqueness => true,
+                        :format => {:with => EMAIL_REGEX}
+
   validates :name,      :presence => true
 
   validates :locale,    :presence => true,
                         :inclusion => { :in => %w(en tr ru)},
                         :length => { :is => 2 }
 
-  def privileges
+  validates :password,  :presence => {:on => :create},
+                        :confirmation => true
+
+  validates :password_confirmation, :presence => true
+
+  def get_privileges
     privileges = Array.new
 
     self.roles.each do |r|
@@ -22,10 +30,10 @@ class User < TenantScopedModel
     privileges
   end
 
-  def apps
+  def get_apps
     apps = Array.new
 
-    self.privileges.each do |p|
+    self.get_privileges.each do |p|
       p.apps.each do |a|
         apps << a unless apps.include?(a)
       end
