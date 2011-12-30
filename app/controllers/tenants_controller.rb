455c453ab86cdb505957e2660641bdd70c6f33a1
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-12-30 12:00:45 +0200

    Issue #38 improve tenants controller bootstrap method

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,8 +1,16 @@
 class TenantsController < ApplicationController
+  skip_before_filter :check_authorize, only: [:bootstrap]
 
-  def register
+  def bootstrap
     @@response[request_forgery_protection_token] = form_authenticity_token
     @@response['tenant'] = Tenant.select('id, host, name').find_by_host!(request.host)
-    render json: @@response, callback: params[:callback]
+    @@response['locale'] = {default_locale: I18n.locale, available_locales: I18n.available_locales}
+    unless session[:user_id].nil?
+      user = User.select("name, email").find(session[:user_id])
+      user[:apps] = User.find(session[:user_id]).get_apps
+      user[:privileges] = User.find(session[:user_id]).get_privileges
+      @@response['user'] = user
+    end
+    render json: @@response
   end
 end
