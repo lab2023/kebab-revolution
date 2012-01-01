0c03f50d1c02c4ef0441f2d4ed69dc93fba1a89a
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-02 00:49:23 +0200

    typo fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
 
   # POST/sessions
   def create
+    # KBBTODO status code
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
