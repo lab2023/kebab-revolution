0999820e036bb1e9b81d5930f1d79cfc6994ea16
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-03 00:02:55 +0200

    Issue #55 add paypal-recurring gem to lib

---
+++ config/initializers/paypal.rb
@@ -0,0 +1,8 @@
+require "paypal/recurring"
+
+PayPal::Recurring.configure do |config|
+  config.sandbox   = true
+  config.username  = "seller_1325467709_biz_api1.lab2023.com"
+  config.password  = "1325467734"
+  config.signature = "AFcWxV21C7fd0v3bYYYRCpSSRl31ABVhWd46KkoIPmVEUIkqX4jB0v2i"
+end
