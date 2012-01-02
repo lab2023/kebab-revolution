c85e79282bc762b4d1a557573c8416496f520fd3
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-02 02:33:25 +0200

    Issue #56 Remove translate apps name from migrations and models

--- db/migrate/20111218194504_create_apps.rb
+++ db/migrate/20111218194504_create_apps.rb
@@ -1,16 +1,9 @@
 class CreateApps < ActiveRecord::Migration
   def up
-
     create_table :apps do |t|
       t.string :sys_name
       t.string :sys_department
     end
-
-    # add role_id not null
-    execute <<-SQL
-      ALTER TABLE  `app_translations` CHANGE  `app_id`  `app_id` INT( 11 ) NOT NULL
-    SQL
-
   end
 
   def down
