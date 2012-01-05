059eb2463103fd8f396be2e9c6e9486078c507c0
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-06 01:44:28 +0200

    Issue #68 remove i18n for Role name

--- app/models/role.rb
+++ app/models/role.rb
@@ -9,5 +9,4 @@ class Role < TenantScopedModel
   has_and_belongs_to_many :users
   has_and_belongs_to_many :privileges
   validates   :name, :presence => true
-  translates  :name, :fallbacks_for_empty_translations => true
 end
