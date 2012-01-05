96f35f09d6e0b6d37411d6f7e89be01bda379468
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-06 01:47:26 +0200

    Issue #52 #55 tenant register, paypal integration

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -6,8 +6,8 @@
 
 # Tenants Controller
 class TenantsController < ApplicationController
-  skip_before_filter  :authenticate,  only: [:create, :valid_host, :bootstrap]
-  skip_before_filter  :tenant,        only: [:create, :valid_host]
+  skip_before_filter  :tenant,        only: [:create, :valid_host, :paypal_credentials]
+  skip_before_filter  :authenticate
   skip_before_filter  :authorize
 
   # GET/tenants/bootstrap
@@ -26,14 +26,51 @@ class TenantsController < ApplicationController
 
   # POST/tenants
   def create
-    @tenant = Tenant.new(params[:tenant])
-    if @tenant.save
-      render json: @@response, status: :created
-    else
-      @@response[:success] = false
-      @tenant.errors.each { |a, m| add_error a, m}
-      render json: @@response, status: :unprocessable_entity
+
+    Tenant.transaction do
+
+      @plan = Plan.find(params[:plan][:id])
+      @tenant = Tenant.new(params[:tenant])
+
+      if @tenant.save
+        Tenant.current = @tenant
+
+        @user   = User.new(params[:user])
+        @user.tenant = @tenant
+        @user.roles << tenant_initial_data(params[:user][:locale])
+
+        if @user.save
+          @subscription = Subscription.new
+          @subscription.user = @user
+          @subscription.tenant = @tenant
+          @subscription.plan = @plan
+          @subscription.price = @plan.price
+          @subscription.payment_period = 0
+          @subscription.next_payment_date = Time.zone.now + 1.months
+
+          if @subscription.save
+            login @user, params[:user][:password]
+            status = :created
+          else
+            @tenant.delete
+            @user.delete
+            @subscription.valid?
+            @@response[:sub_errors] = @subscription.errors
+            status = :unprocessable_entity
+          end
+
+        else
+          @tenant.delete
+          @user.delete
+          @@response[:user_error] = @user.errors
+          status = :unprocessable_entity
+        end
+      else
+        @@response[:success] = false
+        status = :unprocessable_entity
+      end
     end
+    render json: @@response, status: status
   end
 
   # GET/tenants/valid_host?host=host_name
@@ -45,4 +82,20 @@ class TenantsController < ApplicationController
       render json: @@response
     end
   end
+
+  # GET/tenants/paypal_credentials
+  def paypal_credentials
+    if Plan.find(params[:plan][:id]) != nil && (1 < params[:plan][:id].to_i)
+      checkout_url = paypal_credential Plan.find(params[:plan][:id])
+      if checkout_url != false
+        @@response[:url] = checkout_url
+      else
+        @@response[:success] = false
+      end
+    else
+      @@response[:success] = false
+    end
+    render json: @@response
+  end
+
 end
