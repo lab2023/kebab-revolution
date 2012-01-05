e85c4f7f77ec3e8ff51b709dc39f95184ec8dffc
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-06 01:39:41 +0200

    typo fixed

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -9,7 +9,7 @@ class Plan < ActiveRecord::Base
   has_many :subscriptions
 
   validates :name,        :presence => true
-  validates :price,      :presence => true,
+  validates :price,       :presence => true,
                           :numericality => {:greater_than_or_equal_to => 0}
   validates :user_limit,  :presence => true,
                           :numericality => {:greater_than => 0, :only_integer => true}
