5acd7f1ff562b54e249f9c0bb3c6692b3ce5177d
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-01-14 17:03:45 +0200

    Issue #73 refactor notifier

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -17,26 +17,23 @@ class Subscription < ActiveRecord::Base
   validates :price,          :presence => {:on => :create}
   validates :payment_period, :numericality => { :greater_than => 0 }
 
-  scope :commercial, where("subscriptions.price > 0")
-  scope :free,       where("subscriptions.price = 0")
+  scope :commercial,                where("subscriptions.price > 0")
+  scope :free,                      where("subscriptions.price = 0")
+  scope :with_recurring_profile,    where("paypal_recurring_payment_profile_token IS NOT NULL")
+  scope :without_recurring_profile, where("paypal_recurring_payment_profile_token IS NULL")
+  scope :notifier,                  commercial.joins(:user, :plan, :tenant).where("tenants.passive_at IS NULL ") \
+                                    .select("tenants.id as tenant_id, users.email, users.name as user_name, users.locale, subscriptions.price, subscriptions.next_payment_date, plans.name as plan_name")
 
   def self.find_trials_without_recurring_profile(next_payment_date = 10.days.from_now)
-    commercial.select("tenants.id as tenant_id, users.email, users.name as user_name, users.locale, subscriptions.price, subscriptions.next_payment_date, plans.name as plan_name") \
-                  .joins(:user, :plan, :tenant)
-                  .where("tenants.passive_at IS NULL AND paypal_recurring_payment_profile_token IS NULL AND next_payment_date > ? AND next_payment_date < ?", \
-                  next_payment_date.beginning_of_day, next_payment_date.end_of_day)
+    notifier.without_recurring_profile.where("next_payment_date > ? AND next_payment_date < ?", next_payment_date.beginning_of_day, next_payment_date.end_of_day)
   end
 
   # Find due trials to cancel account
   def self.find_finished_trials
-    commercial.select("tenants.id as tenant_id, users.email, users.name as user_name, users.locale, subscriptions.price, subscriptions.next_payment_date, plans.name as plan_name") \
-                  .joins(:user, :plan, :tenant)
-                  .where("tenants.passive_at IS NULL AND paypal_recurring_payment_profile_token IS NULL AND next_payment_date < ?", 1.days.ago.end_of_day)
+    notifier.without_recurring_profile.where("next_payment_date < ?", 1.days.ago.end_of_day)
   end
 
   def self.find_payment_failures(next_payment_date = 10.days.ago)
-    commercial.select("tenants.id as tenant_id, users.email, users.name as user_name, users.locale, subscriptions.price, subscriptions.next_payment_date, plans.name as plan_name") \
-              .joins(:user, :plan, :tenant)
-              .where("tenants.passive_at IS NULL AND next_payment_date > ? AND next_payment_date < ?", next_payment_date.beginning_of_day, next_payment_date.end_of_day)
+    notifier.with_recurring_profile.where("next_payment_date < ?", next_payment_date.end_of_day)
   end
 end
