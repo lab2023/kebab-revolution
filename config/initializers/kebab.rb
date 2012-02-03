e816df9645cf9166a22a7c94fb0692b3cadefefd
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-02-03 16:41:39 +0200

    remove lab2023

--- config/initializers/kebab.rb
+++ config/initializers/kebab.rb
@@ -4,5 +4,5 @@ Kebab.configure do |config|
   config.application_name = "Kebab Project"
   config.application_url = "kebab.local"
   config.invoice_no = "108536" #must be integer
-  config.invalid_tenant_names = %w(www help support api apps status blog lab2023 static)
+  config.invalid_tenant_names = %w(www help support api apps status blog static)
 end
