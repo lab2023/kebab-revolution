342c0f44c71933b7f10c1a931705a407534cda6a
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-02-04 16:25:00 +0200

    Issue #105 add application email

--- config/initializers/kebab.rb
+++ config/initializers/kebab.rb
@@ -3,6 +3,7 @@ require "kebab"
 Kebab.configure do |config|
   config.application_name = "Kebab Project"
   config.application_url = "kebab.local"
+  config.application_email = "info@kebab-project.com"
   config.invoice_no = "108536" #must be integer
   config.invalid_tenant_names = %w(www help support api apps status blog static)
 end
--- lib/kebab.rb
+++ lib/kebab.rb
@@ -5,6 +5,7 @@ module Kebab
  class << self
    attr_accessor :application_name
    attr_accessor :application_url
+   attr_accessor :application_email
    attr_accessor :invoice_no
    attr_accessor :invalid_tenant_names
  end
