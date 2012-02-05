5244fce8150cf0ac43cfc984360c9c2d0d26bbd2
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-02-06 01:35:49 +0200

    #89 open logger.rb until update rails 3.2.1

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,23 +1,23 @@
 #
 # ActiveSupport patches
 ##
-#module ActiveSupport
-#
-#  # Format the buffered logger with timestamp/severity info.
-#  class BufferedLogger
-#    NUMBER_TO_NAME_MAP  = {0=>'DEBUG', 1=>'INFO', 2=>'WARN', 3=>'ERROR', 4=>'FATAL', 5=>'UNKNOWN'}
-#    NUMBER_TO_COLOR_MAP = {0=>'0;37', 1=>'32', 2=>'33', 3=>'31', 4=>'31', 5=>'37'}
-#
-#    def add(severity, message = nil, progname = nil, &block)
-#      return if @level > severity
-#      sevstring = NUMBER_TO_NAME_MAP[severity]
-#      color     = NUMBER_TO_COLOR_MAP[severity]
-#
-#      message = (message || (block && block.call) || progname).to_s
-#      message = "\033[0;37m#{Time.now.to_s(:db)}\033[0m [\033[#{color}m" + sprintf("%-5s","#{sevstring}") + "\033[0m] pid:#{$$} \n #{message.strip} \n\n" unless message[-1] == ?\n
-#      buffer << message
-#      auto_flush
-#      message
-#    end
-#  end
-#end
+module ActiveSupport
+
+  # Format the buffered logger with timestamp/severity info.
+  class BufferedLogger
+    NUMBER_TO_NAME_MAP  = {0=>'DEBUG', 1=>'INFO', 2=>'WARN', 3=>'ERROR', 4=>'FATAL', 5=>'UNKNOWN'}
+    NUMBER_TO_COLOR_MAP = {0=>'0;37', 1=>'32', 2=>'33', 3=>'31', 4=>'31', 5=>'37'}
+
+    def add(severity, message = nil, progname = nil, &block)
+      return if @level > severity
+      sevstring = NUMBER_TO_NAME_MAP[severity]
+      color     = NUMBER_TO_COLOR_MAP[severity]
+
+      message = (message || (block && block.call) || progname).to_s
+      message = "\033[0;37m#{Time.now.to_s(:db)}\033[0m [\033[#{color}m" + sprintf("%-5s","#{sevstring}") + "\033[0m] pid:#{$$} \n #{message.strip} \n\n" unless message[-1] == ?\n
+      buffer << message
+      auto_flush
+      message
+    end
+  end
+end
