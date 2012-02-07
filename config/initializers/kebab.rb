88b366a17bf1506c0d020bbf21b380636774ccd2
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-02-08 00:36:16 +0200

    #116 Add Rails.env Kebab.application_url

--- config/initializers/kebab.rb
+++ config/initializers/kebab.rb
@@ -2,7 +2,7 @@ require "kebab"
 
 Kebab.configure do |config|
   config.application_name = "Kebab Project"
-  config.application_url = "kebab.local"
+  config.application_url = (Rails.env == 'production' ? "kebab.com" : "kebab.local")
   config.application_email = "info@kebab-project.com"
   config.invoice_no = "108536" #must be integer
   config.invalid_tenant_names = %w(www help support api apps status blog static)
