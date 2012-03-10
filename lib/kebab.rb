6b9fbb00e2da2f3da5b4c5696a6fe8c1bae6bdd8
onurozgurozkan <onur.ozgur.ozkan@lab2023.com>
2012-03-10 22:26:34 +0200

    add new variables

--- lib/kebab.rb
+++ lib/kebab.rb
@@ -2,16 +2,19 @@
 # KBBTODO test
 # Kebab Module
 module Kebab
- class << self
-   attr_accessor :application_name
-   attr_accessor :application_url
-   attr_accessor :application_email
-   attr_accessor :invoice_no
-   attr_accessor :invalid_tenant_names
- end
+  class << self
+    attr_accessor :application_version
+    attr_accessor :application_name
+    attr_accessor :application_url
+    attr_accessor :application_email
+    attr_accessor :application_support_email
+    attr_accessor :invoice_no
+    attr_accessor :invalid_tenant_names
+    attr_accessor :blowfish_password
+  end
 
- # Configure
- def self.configure(&block)
-   yield Kebab
- end
+  # Configure
+  def self.configure(&block)
+    yield Kebab
+  end
 end
