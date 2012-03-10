e24c072b33ae601ae77172178adc67f732212b8d
onurozgurozkan <onur.ozgur.ozkan@lab2023.com>
2012-03-10 22:24:38 +0200

    remove feedbacj

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,5 +6,5 @@ ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
 #   inflect.irregular 'person', 'people'
-   inflect.uncountable %w( feedback )
+#   inflect.uncountable %w( feedback )
 end
