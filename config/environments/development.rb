2bff9b8c5d11c6a2ed637339c06dfeef16b76e9d
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2012-04-27 11:41:42 +0300

    #89 update rails 3.2.3

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,13 @@ KebabServerRor::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
 
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,6 +24,9 @@ KebabServerRor::Application.configure do
   # Disable request forgery protection in test environment
   config.action_controller.allow_forgery_protection    = false
 
+  # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
+  config.assets.allow_debugging = true
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
